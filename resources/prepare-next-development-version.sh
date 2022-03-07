2e3b656bf8b374a4aab1effbbf84ce5229a0d135
Remco Westerhoud <remco@westerhoud.nl>
2022-03-07 15:53:11 +0100

    feat: configure git branch
    This enables users to add a branch as input to this action. When passed this branch will be used when updating the versions in maven.
    
    This change gives users more freedom over their release process. For instance, if I want to support multiple versions and have different branches for managing this.
    
    Imagine the following branches:
    1. main
    2. stable/1.0
    3. stable/1.1
    
    When I want to do a patch release on version 1.0, I want the versions to be updated on the stable/1.0 branch. I'd set the `branch` input to `stable/1.0` to achieve this.
    Without this change making a new patch release for version 1.0 will result in an update of the main branch. Where the versions would've been 1.1 before, they'd be updated to 1.0 because of this patch.

--- resources/prepare-next-development-version.sh
+++ resources/prepare-next-development-version.sh
@@ -2,17 +2,18 @@
 
 
 RELEASE_VERSION=$2
-DEFAULT_BRANCH=$1
+BRANCH=$1
 MAVEN_ADDITIONAL_OPTIONS=$3
 
 [ $# != 3 ] && echo "::error::prepare-next-development-version needs exactly 3 arguments." && exit 1
 test -z "${RELEASE_VERSION}" && echo "::debug::Skipping Release because release-version is unset" && exit 0
-test -z "${DEFAULT_BRANCH}" && echo "::error::Default branch needs to be passed" && exit 1
+test -z "${BRANCH}" && echo "::error::Default branch needs to be passed" && exit 1
 
 git fetch --no-tags
-git checkout "${DEFAULT_BRANCH}"
+git branch "${BRANCH}" "origin/${BRANCH}" || exit 1;
+git checkout "${BRANCH}"
 
-[ "$(git rev-list -n1 "${RELEASE_VERSION}")" != "$(git rev-list -n1 "${DEFAULT_BRANCH}")" ] && echo "${RELEASE_VERSION} not pointing to tip of ${DEFAULT_BRANCH}" && exit 0
+[ "$(git rev-list -n1 "${RELEASE_VERSION}")" != "$(git rev-list -n1 "${BRANCH}")" ] && echo "${RELEASE_VERSION} not pointing to tip of ${BRANCH}" && exit 0
 
 # Commit the release version change in the pom.xml
 git add ./**pom.xml
